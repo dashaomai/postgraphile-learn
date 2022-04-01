623efb5c28cb3bc46c751633081781d7482b9e85
Buddy.Jiang(江波达) <buddy.jiang@xsg.com.tw>
2022-04-01 10:58:52 +0800

    doc: 为 learn.person 表添加对应的注释，以便 graphile 自动在 ide 中导出注释

---
+++ migrations/sqls/20220401025050-create-person-table-comments-up.sql
@@ -0,0 +1,5 @@
+COMMENT ON TABLE learn.person IS 'A user of lashcard app';
+COMMENT ON COLUMN learn.person.id IS 'The primary unique identifier for the person.';
+COMMENT ON COLUMN learn.person.first_name IS 'The persons first name.';
+COMMENT ON COLUMN learn.person.last_name IS 'The persons last name.';
+COMMENT ON COLUMN learn.person.created_at IS 'The time this person was created.';
