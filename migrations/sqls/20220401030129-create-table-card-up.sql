1fa541a9773eda6b9c613442ed29e22c5a913a5a
Buddy.Jiang(江波达) <buddy.jiang@xsg.com.tw>
2022-04-01 11:28:02 +0800

    fix: 使用智能标签，禁止通过 graphql 对某些字段进行修改

--- migrations/sqls/20220401030129-create-table-card-up.sql
+++ migrations/sqls/20220401030129-create-table-card-up.sql
@@ -19,10 +19,17 @@ COMMENT ON TABLE learn.card IS 'An individual persons flash card.';
 COMMENT ON COLUMN learn.card.id IS 'The primary unique identifier for the flash card.';
 COMMENT ON COLUMN learn.card.question IS 'The question to prompt the person.';
 COMMENT ON COLUMN learn.card.answer IS 'The answer to the question.';
-COMMENT ON COLUMN learn.card.review_after IS 'The next time the card should be reviewed.';
-COMMENT ON COLUMN learn.card.spacing IS 'The current spacing in days between reviews';
-COMMENT ON COLUMN learn.card.ease_factor IS 'The ease at which the answer was last remembered.';
-COMMENT ON COLUMN learn.card.seq IS 'Controls the order in which to review cards.';
 COMMENT ON COLUMN learn.card.archived IS 'Indicates if the card is active or not.';
-COMMENT ON COLUMN learn.card.person_id IS 'The creator/owner of this card.';
-COMMENT ON COLUMN learn.card.created_at IS 'The time this card was created.';
+
+COMMENT ON COLUMN learn.card.review_after IS '@omit create,update
+The next time the card should be reviewed.';
+COMMENT ON COLUMN learn.card.spacing IS '@omit create,update
+The current spacing in days between reviews';
+COMMENT ON COLUMN learn.card.ease_factor IS '@omit create,update
+The ease at which the answer was last remembered.';
+COMMENT ON COLUMN learn.card.seq IS '@omit create,update
+Controls the order in which to review cards.';
+COMMENT ON COLUMN learn.card.person_id IS '@omit create,update
+The creator/owner of this card.';
+COMMENT ON COLUMN learn.card.created_at IS '@omit create,update
+The time this card was created.';
