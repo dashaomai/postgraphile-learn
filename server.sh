8b97845ed589dc80f86a098ace62fbe229369447
Buddy.Jiang(江波达) <buddy.jiang@xsg.com.tw>
2022-04-01 18:05:40 +0800

    conf: 新增查询条件增强插件，便于进行更多条件的查询

--- server.sh
+++ server.sh
@@ -1,7 +1,7 @@
 npx postgraphile \
   -c postgres://postgres:postgres@localhost:15533/pgh_learn \
   --schema learn \
-  --append-plugins @graphile-contrib/pg-simplify-inflector \
+  --append-plugins @graphile-contrib/pg-simplify-inflector,postgraphile-plugin-connection-filter \
   --enhance-graphiql \
   --allow-explain \
   --watch
