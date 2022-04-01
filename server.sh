15767f5be8fb0ece4c05d7335030bff46b5e24ca
Buddy.Jiang(江波达) <buddy.jiang@xsg.com.tw>
2022-04-01 10:50:34 +0800

    conf: 添加 graphile 插件 simplify-inflector 并启用，同时添加 ide 增强及分析功能

--- server.sh
+++ server.sh
@@ -1,3 +1,6 @@
 npx postgraphile \
   -c postgres://postgres:postgres@localhost:15533/pgh_learn \
-  --schema learn
+  --schema learn \
+  --append-plugins @graphile-contrib/pg-simplify-inflector \
+  --enhance-graphiql \
+  --allow-explain
