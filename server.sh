3dbd61d8380acdb99cd3e07ab782a9b1f907c00d
Buddy.Jiang(江波达) <buddy.jiang@xsg.com.tw>
2022-04-02 10:42:36 +0800

    feat: 创建不同的 role，用于为登录前后的用户，提供不同的控制权限

--- server.sh
+++ server.sh
@@ -1,7 +1,9 @@
 npx postgraphile \
-  -c postgres://postgres:postgres@localhost:15533/pgh_learn \
+  -c postgres://learn_postgraphile:secret_password@localhost:15533/pgh_learn \
   --schema learn \
   --append-plugins @graphile-contrib/pg-simplify-inflector,postgraphile-plugin-connection-filter \
   --enhance-graphiql \
   --allow-explain \
-  --watch
+  --watch \
+  --owner-connection postgres://postgres:postgres@localhost:15533/pgh_learn \
+  --default-role learn_anonymous
